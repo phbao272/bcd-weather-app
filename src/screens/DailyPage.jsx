9f29c6a947b63d7189c7f75f10c6a8874037106d
phbao272 <pqbao27@gmail.com>
2022-03-31 22:06:15 +0700

    update DailyPage

--- src/screens/DailyPage.jsx
+++ src/screens/DailyPage.jsx
@@ -1,69 +1,134 @@
-import React from 'react';
-import { TouchableOpacity, StyleSheet, Image, ScrollView } from 'react-native';
-import { Layout, Text, Icon, Avatar } from '@ui-kitten/components';
-import { useNavigation } from '@react-navigation/native';
+import React from 'react'
+import {
+    TouchableOpacity,
+    StyleSheet,
+    Image,
+    ScrollView,
+    Dimensions,
+} from 'react-native'
+import { Layout, Text, Icon, Avatar } from '@ui-kitten/components'
+import { useNavigation } from '@react-navigation/native'
 
-import { BackIcon } from '../components/icons';
-import Section, { SectionBody, SectionTitle } from '../components/Section';
+import { BackIcon } from '../components/icons'
+import Section, { SectionBody, SectionTitle } from '../components/Section'
 
-import { ConvertUnixTimeToUTC } from '../utils/index';
+import { ConvertUnixTimeToUTC } from '../utils/index'
 
-import globalStyles from '../constants/index';
+import globalStyles from '../constants/index'
 
-const DailyItem = (isToday = false, iconUrl, rainChanceNight, rainChanceDay) => {
+const screen = Dimensions.get('screen')
+
+const DailyItem = (
+    isToday = false,
+    iconUrl,
+    rainChanceNight,
+    rainChanceDay,
+) => {
     return (
         <Layout style={styles.container}>
-            <Layout style={[styles.flexRow, { alignItems: 'center', marginBottom: 4 }]}>
-                <Layout style={{ flex: 2, justifyContent: 'center', alignItems: 'center' }}>
-                    <Avatar source={require(`../../assets/weather-icon/rain.png`)} />
+            <Layout
+                style={[
+                    styles.flexRow,
+                    { alignItems: 'center', marginBottom: 4 },
+                ]}
+            >
+                <Layout
+                    style={{
+                        flex: 2,
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar
+                        source={require(`../../assets/weather-icon/rain.png`)}
+                    />
                 </Layout>
                 <Layout style={{ flex: 8 }}>
-                    <Text style={styles.textStyle}>{ConvertUnixTimeToUTC(1648539456, 'dddd, Do MMMM')}</Text>
+                    <Text style={styles.textStyle}>
+                        {ConvertUnixTimeToUTC(1648539456, 'dddd, Do MMMM')}
+                    </Text>
                     <Text style={[styles.textStyle]}>26&#176;C/22&#176;C</Text>
                 </Layout>
             </Layout>
-            <Layout style={[styles.flexRow, { alignItems: 'flex-start', justifyContent: 'center' }]}>
-                <Layout style={{ flex: 2, justifyContent: 'center', alignItems: 'center' }}>
+            <Layout
+                style={[
+                    styles.flexRow,
+                    { alignItems: 'flex-start', justifyContent: 'center' },
+                ]}
+            >
+                <Layout
+                    style={{
+                        flex: 2,
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}
+                >
                     <Text style={{ color: '#499bf1' }}>67%</Text>
                 </Layout>
                 <Layout style={{ flex: 8 }}>
                     {isToday ? (
-                        <Text>Hôm nay - Có mây. Gió đông với vận tốc 11km/h. Khả năng xảy ra mưa {rainChanceDay}%.</Text>
+                        <Text>
+                            Hôm nay - Có mây. Gió đông với vận tốc 11km/h. Khả
+                            năng xảy ra mưa {rainChanceDay}%.
+                        </Text>
                     ) : (
-                        <Text>Sáng - Có mây. Gió đông với vận tốc 11km/h. Khả năng xảy ra mưa {rainChanceDay}%.</Text>
+                        <Text>
+                            Sáng - Có mây. Gió đông với vận tốc 11km/h. Khả năng
+                            xảy ra mưa {rainChanceDay}%.
+                        </Text>
                     )}
                     {isToday ? (
-                        <Text>Tối nay - Mưa. Gió đông - đông nam với vận tốc 13km/h. Khả năng xảy ra mưa {rainChanceNight}%.</Text>
+                        <Text>
+                            Tối nay - Mưa. Gió đông - đông nam với vận tốc
+                            13km/h. Khả năng xảy ra mưa {rainChanceNight}%.
+                        </Text>
                     ) : (
-                        <Text>Tối - Mưa. Gió đông - đông nam với vận tốc 13km/h. Khả năng xảy ra mưa {rainChanceNight}%.</Text>
+                        <Text>
+                            Tối - Mưa. Gió đông - đông nam với vận tốc 13km/h.
+                            Khả năng xảy ra mưa {rainChanceNight}%.
+                        </Text>
                     )}
                 </Layout>
             </Layout>
         </Layout>
-    );
-};
+    )
+}
 
 const DailyPage = () => {
-    const navigation = useNavigation();
+    const navigation = useNavigation()
 
     const handleGoBack = () => {
-        navigation.goBack();
-    };
+        navigation.goBack()
+    }
 
     return (
         <Layout style={globalStyles.container}>
             <Section>
                 <SectionTitle>
-                    <Layout style={[globalStyles.flexRowCenterAlign, styles.containerFixedTop]}>
-                        <TouchableOpacity onPress={handleGoBack} activeOpacity={0.7}>
+                    <Layout
+                        style={[
+                            globalStyles.flexRowCenterAlign,
+                            styles.containerFixedTop,
+                        ]}
+                    >
+                        <TouchableOpacity
+                            onPress={handleGoBack}
+                            activeOpacity={0.7}
+                        >
                             <BackIcon />
                         </TouchableOpacity>
-                        <Text style={{ marginLeft: 24, fontSize: 20 }}>8 Ngày tiếp</Text>
+                        <Text style={{ marginLeft: 24, fontSize: 20 }}>
+                            8 Ngày tiếp
+                        </Text>
                     </Layout>
                 </SectionTitle>
                 <SectionBody>
                     <ScrollView>
-                        <DailyItem isToday="true" rainChanceDay="12" rainChanceNight="37" />
+                        <DailyItem
+                            isToday="true"
+                            rainChanceDay="12"
+                            rainChanceNight="37"
+                        />
                         <DailyItem rainChanceDay="12" rainChanceNight="37" />
                         <DailyItem rainChanceDay="12" rainChanceNight="37" />
                         <DailyItem rainChanceDay="12" rainChanceNight="37" />
@@ -75,8 +140,8 @@ const DailyPage = () => {
                 </SectionBody>
             </Section>
         </Layout>
-    );
-};
+    )
+}
 
 const styles = StyleSheet.create({
     flexRow: {
@@ -96,7 +161,7 @@ const styles = StyleSheet.create({
 
     textStyle: {
         fontSize: 18,
-        fontWeight: 500,
+        fontWeight: '500',
     },
 
     container: {
@@ -112,6 +177,6 @@ const styles = StyleSheet.create({
         height: screen.width,
         borderRadius: 12,
     },
-});
+})
 
-export default DailyPage;
+export default DailyPage
