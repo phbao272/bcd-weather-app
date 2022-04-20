fd592089128cb52795eb898041ad7d0b227d366d
cuongdng <cuongdn.sun@gmail.com>
2022-04-20 23:39:24 +0700

    Completed: Hourly-page

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,63 +1,50 @@
-import moment from 'moment'
-import _ from 'lodash'
+import moment from 'moment';
+import _ from 'lodash';
 
-moment.locale('vi')
+moment.locale('vi');
 
 moment.updateLocale('vi', {
     weekdays: ['CN', 'T.2', 'T.3', 'T.4', 'T.5', 'T.6', 'T.7'],
-    months: [
-        'THÁNG 1',
-        'THÁNG 2',
-        'THÁNG 3',
-        'THÁNG 4',
-        'THÁNG 5',
-        'THÁNG 6',
-        'THÁNG 7',
-        'THÁNG 8',
-        'THÁNG 9',
-        'THÁNG 10',
-        'THÁNG 11',
-        'THÁNG 12',
-    ],
-})
+    months: ['THÁNG 1', 'THÁNG 2', 'THÁNG 3', 'THÁNG 4', 'THÁNG 5', 'THÁNG 6', 'THÁNG 7', 'THÁNG 8', 'THÁNG 9', 'THÁNG 10', 'THÁNG 11', 'THÁNG 12'],
+});
 
 // 'dddd, Do MMMM'
 // 'hh:mm'
 const ConvertUnixTimeToUTC = (time, format) => {
-    return time ? moment(time * 1000).format(format) : ''
-}
+    return time ? moment(time * 1000).format(format) : '';
+};
 
 const ConvertKToC = (kelvin) => {
-    return kelvin ? (kelvin - 273.15).toFixed() : ''
-}
+    return kelvin ? (kelvin - 273.15).toFixed() : '';
+};
 
 const ConvertVisibility = (visibility) => {
-    return visibility ? (visibility / 1000).toFixed() : ''
-}
+    return visibility ? (visibility / 1000).toFixed() : '';
+};
 
 const ConvertWindSpeed = (speed) => {
-    return speed ? (speed * 3.6).toFixed() : ''
-}
+    return speed ? (speed * 3.6).toFixed() : '';
+};
 
 const ConvertWindDeg = (deg) => {
-    let res = ''
+    let res = '';
 
     if (deg >= 22.5 && deg <= 112.5) {
-        res = 'đông - đông bắc'
+        res = 'đông - đông bắc';
     } else if (deg > 112.5 && deg < 202.5) {
-        res = 'nam - đông nam'
+        res = 'nam - đông nam';
     } else if (deg > 202.5 && deg < 292.5) {
-        res = 'tây - tây nam'
+        res = 'tây - tây nam';
     } else {
-        res = 'bắc - tây bắc'
+        res = 'bắc - tây bắc';
     }
 
-    return res
-}
+    return res;
+};
 
 const ConvertPop = (value) => {
-    return value ? (value * 100).toFixed() : ''
-}
+    return value ? (value * 100).toFixed() : '0';
+};
 
 // export const calcAQI = (arr, value) => {
 //     const max = _.max(arr)
@@ -85,11 +72,4 @@ const ConvertPop = (value) => {
 //     console.log({ weight, nowCast })
 // }
 
-export {
-    ConvertKToC,
-    ConvertUnixTimeToUTC,
-    ConvertVisibility,
-    ConvertWindSpeed,
-    ConvertWindDeg,
-    ConvertPop,
-}
+export { ConvertKToC, ConvertUnixTimeToUTC, ConvertVisibility, ConvertWindSpeed, ConvertWindDeg, ConvertPop };
